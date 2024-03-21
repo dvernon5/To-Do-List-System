5a06a599326e63e61d7c8e754111514b2b4f28dd
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-03-21 11:06:24 -0700

    Create ToDoListManagementSystem.h declaration to launch the todo list management system

---
+++ ToDoListManagementSystem.h
@@ -0,0 +1,13 @@
+#ifndef ToDoListManagementSystem_h
+#define ToDoListManagementSystem_h
+#include "BuildToDoListManagementSystem.h"
+
+class ToDoListManagementSystem {
+  public:
+    void LaunchToDoListManagementSystem();
+  
+  private:
+    BuildToDoListManagementSystem built_todo_list;
+};
+
+#endif /* ToDoListManagementSystem_h */
